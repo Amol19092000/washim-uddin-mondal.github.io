fb18660765d3eddbff60ee7e6414538868410911
washim-uddin-mondal <74007561+washim-uddin-mondal@users.noreply.github.com>
2022-06-08 15:09:40 -0400

    update-summary

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -28,7 +28,7 @@ function loadNews(){
 
 function loadJournalPublications(){
     dataLocation = '../Data/journalData.json';
-    columns = Array("Year", "Title");
+    columns = Array("No.", "Title");
     tableID = "journalTable";
 
     loadPublicationData(dataLocation, columns, tableID);
@@ -36,10 +36,70 @@ function loadJournalPublications(){
 
 function loadConferencePublications(){
     dataLocation = '../Data/conferenceData.json';
-    columns = Array("Year", "Title");
+    columns = Array("No.", "Title");
     tableID = "conferenceTable";
 
     loadPublicationData(dataLocation, columns, tableID);
+}
+
+function loadSummaryPublications(){
+    dataLocation = '../Data/journalData.json';
+    columns = Array("Journal", "Count");
+    tableID = "summaryTableJournal";
+
+    loadSummaryData("Journal", dataLocation, columns, tableID);
+
+    dataLocation = '../Data/conferenceData.json';
+    columns = Array("Conference", "Count");
+    tableID = "summaryTableConference";
+
+    loadSummaryData("Conference", dataLocation, columns, tableID);
+}
+
+function loadSummaryData(type, dataLocation, columns, tableID){
+    var pageRequest = new XMLHttpRequest();
+    pageRequest.open('GET', dataLocation);
+    
+    pageRequest.onload = function () {
+        var jsonTempContent = JSON.parse(pageRequest.responseText);
+        var jsonContent = [];
+
+        for(i=0; i < jsonTempContent.length; i++){
+            ifOld = false;
+            for(j=0; j < jsonContent.length; j++){
+                if(jsonContent[j][type] == jsonTempContent[i]["Journal/Conference"]){
+                    jsonContent[j]["Count"] += 1;
+                    ifOld = true;
+                    break;
+                }
+            }
+
+            if (ifOld == false){
+                if (type == "Journal"){
+                    jsonContent.push(
+                        {
+                        "Journal": jsonTempContent[i]["Journal/Conference"], 
+                        "Count": 1
+                        }
+                    );
+                }
+                if (type == "Conference"){
+                    jsonContent.push(
+                        {
+                        "Conference": jsonTempContent[i]["Journal/Conference"], 
+                        "Count": 1
+                        }
+                    );
+                }
+
+            }
+        }
+
+        htmlContent = makeHTMLTable(jsonContent, columns);
+        document.getElementById(tableID).innerHTML = htmlContent;
+    }
+
+    pageRequest.send();
 }
 
 function loadPublicationData(dataLocation, columns, tableID){
@@ -50,7 +110,8 @@ function loadPublicationData(dataLocation, columns, tableID){
         var jsonContent = JSON.parse(pageRequest.responseText);
 
         for(i=0; i < jsonContent.length; i++){
-            jsonContent[i]["Title"] = jsonContent[i]["Authors"] + ", \"<a href=" + jsonContent[i]["URL"] + " target=\"_blank\">" + jsonContent[i]["Title"] + "</a>\", <i>" + jsonContent[i]["Journal/Conference"] + "</i>.";
+            jsonContent[i]["Title"] = jsonContent[i]["Authors"] + ", \"<a href=" + jsonContent[i]["URL"] + " target=\"_blank\">" + jsonContent[i]["Title"] + "</a>\", <i>" + jsonContent[i]["Journal/Conference"] + "</i>, " + jsonContent[i]["Year"] + ".";
+            jsonContent[i]["No."] = "["+ (i+1) + "]";
         }
         htmlContent = makeHTMLTable(jsonContent, columns);
         document.getElementById(tableID).innerHTML = htmlContent;
